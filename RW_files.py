d2dc0f0fe617d0bc0afaf3110753c0a641a78736
Killberos Tze <killberos.tze@gmail.com>
2024-04-21 16:52:50 +0200

    some changes

--- RW_files.py
+++ RW_files.py
@@ -18,16 +18,13 @@ import os
 
 class Files_RW():
     hashtags=['#comment','#setup','#data_header','#data_table']
+    split=':='
               
     class container():
         pass
     
-    #to have it here although not used
-    def Add_items(self,text,itemlist,sep):
-        for item in itemlist:
-            text=text+str(item)+sep
-        return text[:-1]
 
+#ini files
     def check_E60_ini(self,dirname,filename,split):
         out=self.container()
         with open(os.path.join(dirname,filename), 'r') as f:
@@ -62,7 +59,23 @@ class Files_RW():
                 tmp=a.split(split)
                 ip_list.append(tmp[-1])
         return ip_list
+    
+    def check_IV_analysis_ini(self,dirname,filename,split):
+        out=self.container()
+        with open(os.path.join(dirname,filename), 'r') as f:
+            for line in f:
+                a=line.strip()
+                tmp=a.split(split)
+                if tmp[0]=='load_file_path':
+                    out.filedir=tmp[-1]
+                if tmp[0]=='database_path':
+                    out.dbdir=tmp[-1]
+                if tmp[0]=='database_file':
+                    out.dbname=tmp[-1]
+                
+        return out
 
+#write to files
     def write_to_file(self,dirname,filename,write):
         with open(os.path.join(dirname,filename),'w') as f:
             for line in write:
@@ -74,6 +87,8 @@ class Files_RW():
                 np.savetxt(f, [line], delimiter='\t', newline='\n', fmt='%s')
             for line in data:
                 np.savetxt(f, [line], delimiter='\t', newline='\n', fmt=fmtlist)
+
+#E60 raw data
 
     def load_dsp(self,filename):
         out=self.container()
@@ -116,7 +131,35 @@ class Files_RW():
         elif out.type=='A':
             out.type='Absorbance'
         return out
+#help functions
+    def process_2col_data(self,data,idx):
+        col1=data[:,idx[0]]
+        col2=data[:,idx[1]]
+        return col1,col2
+    
+    def reset_markers(self,markers,mykey):
+        for key in markers.keys():
+            if key==mykey:
+                markers[key]=1;
+            else:
+                markers[key]=0
+                
+    #this function is obsolete
+    def insert_symbol(self,string_list,symbol):
+        #new_string=''
+        #for item in string_list:
+        #    new_string=new_string+item+symbol
+        #new_string=new_string[0:-1]
+        new_string=symbol.join(string_list)
+        return new_string
+    
+    #to have it here although not used
+    def Add_items(self,text,itemlist,sep):
+        for item in itemlist:
+            text=text+str(item)+sep
+        return text[:-1]
 
+#my TMM
     def process_TMM_header(self,header,*args):
         if args:
             args=args
@@ -134,15 +177,10 @@ class Files_RW():
             pass
         return idx,wlength_units,data_units,error
 
-    def process_2col_data(self,data,idx):
-        col1=data[:,idx[0]]
-        col2=data[:,idx[1]]
-        return col1,col2
-
-    def load_reference_TMM(self,filename,*args,**kwargs):
+    def load_reference_TMM(self,filename):
         out=self.container()
         error=''
-        (comment,setup,header,data,error)=self.read_ihtm_file(filename,**kwargs)
+        (comment,setup,header,data,error)=self.read_ihtm_file(filename)
         if not error:
             idx,out.wlength_units,out.data_units,erorr=self.process_TMM_header(header)
         if not error:
@@ -151,7 +189,7 @@ class Files_RW():
         out.error=error
         return out
 
-
+#my processed E60
     def process_dtsp_header(self, header):
         error=''
         wlength_units=''
@@ -166,10 +204,10 @@ class Files_RW():
         return idx,wlength_units,data_units,data_type,error
 
 
-    def load_dtsp(self,filename,*args,**kwargs):
+    def load_dtsp(self,filename):
         out=self.container()
         error=''
-        (comment,setup,header,data,error)=self.read_ihtm_file(filename,**kwargs)
+        (comment,setup,header,data,error)=self.read_ihtm_file(filename)
         if not error:
             idx,out.wlength_units,out.data_units,out.type,erorr=self.process_dtsp_header(header)
         if not error:
@@ -177,23 +215,86 @@ class Files_RW():
         out.error=error
         return out
 
-    def reset_markers(self,markers,mykey):
-        for key in markers.keys():
-            if key==mykey:
-                markers[key]=1;
-            else:
-                markers[key]=0
-
-    def read_ihtm_file(self,filename,**kwargs):#this should be the same for all files you are creating either in measurements of after processing except for AFM files
+#measured IV files
+    def process_iv_setup(self,setup,*args):
+        measurement_date=''
+        measurement_time=''
+        sample_name=''
+        device_area=''
+        area_units=''
+        if args:
+            split=args[0]
+        else:
+            split=Files_RW.split
+        for item in setup:
+            tmp=item.split(split)
+            if tmp[0]=='measurement_date':
+                measurement_date=tmp[-1].replace('.','')
+            elif tmp[0]=='measurement_time':
+                measurement_time=tmp[-1]
+            elif tmp[0]=='sample_name':
+                sample_name=tmp[-1]
+            elif tmp[0]=='device_area':
+                device_area=float(tmp[-1])
+            elif tmp[0]=='area_units':
+                area_units=tmp[-1]
+        return (measurement_date,measurement_time,sample_name,device_area,area_units)
+                
+                            
+    def process_iv_header(self,header,i,*v):
+        idx=self.container()
+        idx.i_col=header[0].index(i)
+        idx.v_col=[]
+        for item in v:
+            idx.v_col.append(header[0].index(item))
+        
+        v_units=header[1][idx.v_col[-1]]
+        i_units=header[1][idx.i_col]
+        return (idx, v_units, i_units)
+    
+    def process_iv_data(self,table,idx):
+        data=np.array(table)
+        v=data[:,idx.v_col[0]].astype(float)
+        for i in range(1,len(idx.v_col)):
+            v+=data[:,idx.v_col[i]].astype(float)
+        i=data[:,idx.i_col].astype(float)
+        return (v,i)
+               
+    def load_iv_file(self,filename):
+        out=self.container()
+        out.data=self.container()#measured data
+        out.meas=self.container()#data about measurement
+        out.cell=self.container()#data about the cell
+        out.error=''
+        (comment,setup,header,data,error)=self.read_ihtm_file(filename)
+        if error=='':
+            (idx,out.data.v_units,out.data.i_units)=self.process_iv_header(header,'current','voltage')
+            v,i=self.process_iv_data(data,idx)
+            out.data.v,out.data.i=v,-i
+            (out.meas.date,out.meas.time,sample_name,out.cell.area,out.cell.area_units)=self.process_iv_setup(setup,*args)
+            if not out.cell.area_units:
+                #so that nothing changes when you don't have exact area
+                out.cell.area = 1
+                out.cell.area_units = 'cm^2'
+            #(out.cell.area,out.cell.area_units)=Analyze_IV().convert_area_units(area,area_units)
+            #this is only temporary fix
+            #(i,i_units)=Analyze_IV().convert_current_mA(out.data.i,out.data.i_units)  
+            #out.data.cd=i/out.cell.area#area should be read from your file fix it
+            #out.data.cd_units=i_units+'/'+out.cell.area_units#current density
+        else:
+            out.error=error
+        #if table_row!=len(out.data.v):
+        #    out.error='Data file is corrupter.'
+        return out
+    
+#for all of my files
+    def read_ihtm_file(self,filename):#this should be the same for all files you are creating either in measurements of after processing except for AFM files
         comment=[]
         setup=[]
         header=[]
         data=[]
         error='Wrong type of file!'
-        if kwargs:
-            markers={value:0 for value in kwargs.values()}
-        else:
-            markers={item:0 for item in Files_RW.hashtags}
+        markers={item:0 for item in Files_RW.hashtags}
         try:
             with open(filename, 'r') as f:
                 for line in f:
@@ -224,8 +325,9 @@ class Files_RW():
             error='File cannot be read!'
         if header or comment or setup or data:
             error=''
-        return comment, setup, header, np.array(data).astype('float'), error 
+        return comment, setup, header, np.array(data).astype(float), error 
     
+#gwyddion ascii matrix you should write your matrix files like this so you can use sam load funciton 
     def load_ascii_matrix(self,filename):
         out=self.container()
         out.setup=[]
@@ -262,3 +364,77 @@ class Files_RW():
             elif tmp[0]=='# Value units':
                 z_units=tmp[-1].strip()
         return float(x),x_units,float(y),y_units,z_units
+#dta file from prof. Dragisa    
+    def load_dta_file(self,filename):
+        out=self.container()
+        out.data=self.container()
+        out.meas=self.container()
+        out.cell=self.container()
+        out.error=''
+        (comment,header,data,error)=self.read_dta_file(filename)
+        if error=='':
+            (idx,out.data.v_units,out.data.i_units)=self.process_iv_header(header,'Im','Vf','Vu')
+            v,i=self.process_iv_data(data,idx)
+            out.data.v,out.data.i=-v,-i
+            (out.meas.date,out.meas.time,area,area_units,table_row)=self.process_dta_comment(comment)
+            #area is always in cm^2
+            #(out.cell.area,out.cell.area_units)=Analyze_IV().convert_area_units(area,area_units)
+            #current density should be in mA/cm^2
+            #(i,i_units)=Analyze_IV().convert_current_mA(out.data.i,out.data.i_units)       
+            #out.data.cd=i/out.cell.area 
+            #out.data.cd_units=i_units+'/'+out.cell.area_units#current density
+        else:
+            out.error=error
+        if table_row!=len(out.data.v):
+            out.error='Data file is corrupter.'
+        return out
+    
+    def read_dta_file(self,filename):
+        comment=[]
+        data=[]
+        header=[]
+        error=''
+        comment_marker=1#comment text at beginning that is why this is set to negative
+        header_marker=0#set to positive so it wait when table header starts
+        table_marker=0#set to positive to wait when table starts
+        try:
+            with open(filename,'r') as f:
+                for line in f:
+                    if comment_marker:
+                        comment.append(line.strip())
+                    #header of table
+                    if header_marker:
+                        header.append(line.strip().split('\t'))#because of units we need to split
+                    #table itself
+                    if table_marker:
+                        data.append(line.strip().split('\t'))#because of qunatities we need to split
+                    #marker changes
+                    tmp=line.strip().split('\t')
+                    if tmp[0]=='CURVE':
+                        comment_marker=0
+                        header_marker=1
+                    if tmp[0]=='#':
+                        header_marker=0
+                        table_marker=1
+        
+        except:
+            error='File cannot be read.'
+        return (comment,header,data,error)
+
+    def process_dta_comment(self,comment):
+        for line in comment:
+            tmp=line.split('\t')
+            if tmp[0]=='DATE':
+                american_date=tmp[2].split('/')
+                measurement_date=self.insert_symbol([american_date[2],american_date[0],american_date[1]],'')
+            if tmp[0]=='TIME':
+                measurement_time=tmp[2]
+                
+            if tmp[0]=='AREA':
+                device_area=float(tmp[2])
+                area_units=tmp[3].split()[-1][1:-1]
+            if tmp[0]=='CURVE':
+                table_row=int(tmp[2])
+        return (measurement_date,measurement_time,device_area,area_units,table_row)
+
+    
